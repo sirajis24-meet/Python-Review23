4d27eb7c4e770fd0ae5c9ce012d574112d987c39
sirajis24-meet <139681226+sirajis24-meet@users.noreply.github.com>
2023-07-16 17:12:35 +0300

    Update pythonReview.py

---
+++ pythonReview.py
@@ -0,0 +1,23 @@
+def create_youtube_video(title, description):
+	youtube = {title: "", description : "", "likes" : 0 , "dislikes" : 0, 'comments': {}}
+	return youtube
+
+def like(youtube):
+	if likes in youtube:
+		youtube[likes] = youtube[likes+1]
+
+	return youtube
+
+
+def discdlike(youtube):
+	if dislikes in youtube:
+		youtube[dislikes] = youtube[dislikes+1]
+	return youtube
+
+def addcomment(youtube, username, comment_text):
+	youtube["comments"]["username"] = comment_text
+	return youtube
+
+title1 = input("enter title ")
+desc1 =input(" enter desc ")
+print(create_youtube_video(title1, desc1 ))
