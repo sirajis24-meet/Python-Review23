c8dbf260b909b141357923979261b51371b13d7c
sirajis24-meet <139681226+sirajis24-meet@users.noreply.github.com>
2023-07-18 13:38:46 +0300

    o

--- pythonReview.py
+++ pythonReview.py
@@ -1,26 +1,46 @@
 def create_youtube_video(title, description):
-	youtube = {title: "", description : "", "likes" : 0 , "dislikes" : 0, 'comments': {}}
+	youtube = {title: "", description : "", "likes" : 0 , "dislikes" : 51, 'comments': {"o" : "hi"}, "hashtags" : []}
+	youtube["hashtags"].append([input("enter hashtags ")])
+	youtube["hashtags"].append([input("enter hashtags ")])
 	return youtube
 
 def like(youtube):
-	if likes in youtube:
+	if "likes" in youtube:
 		youtube["likes"] = youtube["likes"]+1
 
 	return youtube
 
 
 def dislike(youtube):
-	if dislikes in youtube:
+	if "dislikes" in youtube:
 		youtube["dislikes"] = youtube["dislikes"]+1
 	return youtube
 
 def addcomment(youtube, username, comment_text):
 	youtube['comments'][username] = comment_text
-	return youtube
+	return 
 
-title1 = input("enter title ")
-desc1 =input("enter desc ")
-username1 = input("enter username ")
-comment1=input("enter comment ")
-y = create_youtube_video(title1, desc1 )
-like(y)
+
+def similarity(y1, y2):
+	similar = 0
+	
+	list("hashtags")
+	for i in (hashtags(len(num))):
+		if y1(num[i]) == y2(num):
+			similar + 1
+	similar/len(num)
+	return similar
+
+
+
+
+
+
+y1 = create_youtube_video("the vid","haha") 
+y2 = create_youtube_video("my vid","hihi" )
+for i in range (495):
+	like(y1)
+print(y1)
+print(similarity(y1, y2))
+
+
